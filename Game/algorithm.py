204d8f00397b71e6116abe25f6368105a1b6a923
huichuan <lhc14124908@163.com>
2020-12-27 20:37:45 +0800

    add

--- Game/algorithm.py
+++ Game/algorithm.py
@@ -31,3 +31,43 @@ def minmax_decision(state, game):
 
     print([min_value(game.result(state, a)) for a in game.actions(state)])
     return max(game.actions(state), key=lambda a: min_value(game.result(state, a)))
+
+
+## alpha_beta_search 搜索
+
+def alpha_beta_search(state, game):
+    player = game.to_move(state)
+
+    # Functions used by alpha_beta
+    def max_value(state, alpha, beta):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a), alpha, beta))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    def min_value(state, alpha, beta):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = np.inf
+        for a in game.actions(state):
+            v = max(v, max_value(game.result(state, a), alpha, beta))
+            if v >= beta:
+                return v
+            beta = max(beta, v)
+        return v
+
+    # Body of alpha_beta_search:
+    best_score = -np.inf
+    beta = np.inf
+    best_action = None
+    for a in game.actions(state):
+        v = min_value(game.result(state, a), best_score, beta)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
--- test_game.py
+++ test_game.py
@@ -19,5 +19,13 @@ def test_minmax_decision():
     print(minmax_decision('D', f52) == 'd3')
 
 
+def test_alpha_beta_search():
+    assert alpha_beta_search('A', f52) == 'a1'
+    assert alpha_beta_search('B', f52) == 'b1'
+    assert alpha_beta_search('C', f52) == 'c1'
+    assert alpha_beta_search('D', f52) == 'd3'
+
+
 if __name__ == "__main__":
     test_minmax_decision()
+    test_alpha_beta_search()
