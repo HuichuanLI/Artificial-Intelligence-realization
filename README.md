94d77880aeffbe001cbf2f2e6b11aeac7c950ea3
huichuan <lhc14124908@163.com>
2020-12-28 15:31:50 +0800

    add

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Artificial-Intelligence-realization
-Python code for the book Artificial Intelligence: A Modern Approach. You can use this in conjunction with a course on AI, or for study on your own. 
+
+Python code for the book Artificial Intelligence: A Modern Approach. You can use this in conjunction with a course on
+AI, or for study on your own.
 
 # 搜索算法
 
@@ -7,16 +9,14 @@ Python code for the book Artificial Intelligence: A Modern Approach. You can use
 
 - DFS
 - BFS
-- Uniform 
+- Uniform
 - DFS limited
 - DFS iterative
 - Bidirection Searching(to do)
 
 ![八皇后问题](./photo/2.png)
 
-
 ## 有信息搜索
-
 
 也被称为启发式搜索
 
@@ -41,10 +41,10 @@ h(n) = 从节点n到目标的最低径路代价估计
 - 贪婪搜索
 - A*搜索
 
-
 目前实现了
- - 罗马尼亚问题
- - n皇后问题
+
+- 罗马尼亚问题
+- n皇后问题
 
 ![八皇后问题](./photo/1.png)
 
@@ -54,11 +54,12 @@ h(n) = 从节点n到目标的最低径路代价估计
 
     该系统性是由以下方法得到：在内存中保持一条或多条路径，并且在沿着该路径的每个点上记录哪些已被探索过；目标找到时，该路径也就构成问题的一个解
     然而在许多问题中到达目标的路径是无关紧要的
+
 局部搜索是一种不同于经典搜索的算法，它不介意什么路径
 
     局部搜索算法使用一个当前节点（而不是多条路径），并且通常仅移动到该节点相邻的节点，通常，搜索后不保留该路径
     局部搜索有2个优点：使用很少内存；在大的或者无限（连续）状态空间中，能发现合理的解
-    
+
 - 爬山法
 - 模拟退火
 - 局部束搜索[未完成]
@@ -78,11 +79,10 @@ h(n) = 从节点n到目标的最低径路代价估计
     该算法采用自然进化所派生的技法来生成优化问题的解，例如：遗传、突变、选择以及杂交
     
     该算法开始时具有一组k个随机生成的状态，称为种群，每个单个状态称为个体，每个个体通常由一组字符一般为01字符串，如八皇后的个体
-    
+
 ![八皇后问题](./photo/3.png)
 
-## 对抗搜索(游戏问题)
-
+## 对抗搜索(游戏问题) adversarial search
 
 零和博弈
 
@@ -99,6 +99,31 @@ h(n) = 从节点n到目标的最低径路代价估计
 - minmax算法
 - alpha-beta 算法
 
-
+实现了：TicTacToe,connect4,komodu
 
 ![八皇后问题](./photo/6.png)
+
+
+    Minimax algorithm can select optimal moves by a depth-first enumeration of game tree.
+    
+    Minimax算法可以通过博弈树的深度优先计算选择最佳移动。
+    
+    
+    
+    Alpha–beta algorithm achieves much greater efficiency by pruning irrelevant subtrees.
+    
+    Alpha–beta算法通过剪掉不相关子树来得到更高的效率。
+    
+    
+    
+    Heuristic evaluation function is useful for imperfect real-time decisions of games.
+    
+    启发式评价函数对于博弈的不完全实时决策很有效。
+    
+    
+    
+    Stochastic game is a dynamic game with probabilistic transitions.
+    
+    随机博弈是具有概率转换的动态博弈。
+
+
--- play.py
+++ play.py
@@ -3,7 +3,7 @@
 # @Author : huichuan LI
 # @File : play.py
 # @Software: PyCharm
-from Game import minmax_decision, alpha_beta_player, random_player, TicTacToe, Gomoku
+from Game import minmax_player, minmax_decision, alpha_beta_player, random_player, TicTacToe, Gomoku
 
 from test_game import gen_state
 
@@ -27,43 +27,47 @@ choose = int(choose)
 print("输入你的位置 比如：1,2:")
 
 while True:
-    state = gen_state(to_move='O', x_positions=x_pos,
-                      o_positions=o_pos, h=ttt.h, v=ttt.v)
-    ttt.display(state)
-    position = input("位置:")
-    while "," not in position:
-        print("重新输入")
+    try:
+        state = gen_state(to_move='O', x_positions=x_pos,
+                          o_positions=o_pos, h=ttt.h, v=ttt.v)
+        ttt.display(state)
         position = input("位置:")
-    a, b = map(int, position.split(","))
-    # clear_the_screen = sp.call('clear', shell=True)  # Linux
-    board = ttt.result(state, (a, b))
-    while board == state:
-        print("重新输入")
-        position = input("位置:")
+        while "," not in position:
+            print("重新输入")
+            position = input("位置:")
         a, b = map(int, position.split(","))
+        # clear_the_screen = sp.call('clear', shell=True)  # Linux
+        board = ttt.result(state, (a, b))
+        while board == state:
+            print("重新输入")
+            position = input("位置:")
+            a, b = map(int, position.split(","))
+            board = ttt.result(state, (a, b))
+
+        o_pos.append((a, b))
+        state = gen_state(to_move='X', x_positions=x_pos,
+                          o_positions=o_pos, h=ttt.h, v=ttt.v)
+
+        if ttt.compute_utility(board.board, (a, b), 'O'):
+            ttt.display(state)
+            print("you win")
+            exit()
+
+        if choose == 1:
+            a, b = random_player(ttt, state)
+        elif choose == 2:
+            a, b = minmax_player(ttt, state)
+        else:
+            a, b = alpha_beta_player(ttt, state)
+        x_pos.append((a, b))
+        state = gen_state(to_move='O', x_positions=x_pos,
+                          o_positions=o_pos, h=ttt.h, v=ttt.v)
         board = ttt.result(state, (a, b))
 
-    o_pos.append((a, b))
-    state = gen_state(to_move='X', x_positions=x_pos,
-                      o_positions=o_pos, h=ttt.h, v=ttt.v)
-
-    if ttt.compute_utility(board.board, (a, b), 'O'):
-        ttt.display(state)
-        print("you win")
-        exit()
-
-    if choose == 1:
-        a, b = random_player(ttt, state)
-    elif choose == 2:
-        a, b = minmax_decision(state, ttt)
-    else:
-        a, b = alpha_beta_player(ttt, state)
-    x_pos.append((a, b))
-    state = gen_state(to_move='O', x_positions=x_pos,
-                      o_positions=o_pos, h=ttt.h, v=ttt.v)
-    board = ttt.result(state, (a, b))
-
-    if ttt.compute_utility(board.board, (a, b), 'X'):
-        ttt.display(state)
-        print("you lose")
+        if ttt.compute_utility(board.board, (a, b), 'X'):
+            ttt.display(state)
+            print("you lose")
+            exit()
+    except (ValueError, IndexError, TypeError) as e:
+        print("draw")
         exit()
